d8fd84bfa4f078b37c0d478bf79f363b839b3762
thraneh <thraneh@gmail.com>
2023-05-28 07:01:07 +0200

    update submodules + fix refactor

--- test/fix_new_order_single.cpp
+++ test/fix_new_order_single.cpp
@@ -5,41 +5,56 @@
 #include "roq/debug/fix/message.hpp"
 #include "roq/debug/hex/message.hpp"
 
-#include "roq/fix/new_order_single.hpp"
-
-// note! the FIX Bridge NewOrderSingle template doesn't yet create 2-way bindings
-// #include "roq/fix_bridge/fix/new_order_single.hpp"
-#include "roq/fix_bridge/fix/reject.hpp"
+#include "roq/fix_bridge/fix/new_order_single.hpp"
 
 using namespace std::literals;
 using namespace std::chrono_literals;
 
+namespace {
 using Header = roq::fix::Header;
-using Reject = roq::fix_bridge::fix::Reject;
+using NewOrderSingle = roq::fix_bridge::fix::NewOrderSingle;
+constexpr auto NaN = std::numeric_limits<double>::quiet_NaN();
+};  // namespace
 
 TEST_CASE("fix_new_order_single", "[fix_new_order_single]") {
   std::vector<std::byte> buffer(4096);
-  auto reject = Reject{
-      .ref_seq_num = 1,
-      .text = "failure"sv,
-      .ref_tag_id = {},
-      .ref_msg_type = roq::fix::NewOrderSingle::msg_type,
-      .session_reject_reason = roq::fix::SessionRejectReason::INVALID_MSG_TYPE,
+  auto new_order_single = NewOrderSingle{
+      .cl_ord_id = "123"sv,
+      .no_party_ids = {},
+      .account = "A1",
+      .handl_inst = {},
+      .exec_inst = {},
+      .no_trading_sessions = {},
+      .symbol = "BTC-PERPETUAL"sv,
+      .security_exchange = "deribit"sv,
+      .side = roq::fix::Side::BUY,
+      .transact_time = 1685248384123ms,
+      .order_qty = 1.0,
+      .ord_type = roq::fix::OrdType::LIMIT,
+      .price = 27193.0,
+      .stop_px = NaN,
+      .time_in_force = roq::fix::TimeInForce::GTC,
+      .text = {},
+      .position_effect = {},
+      .max_show = NaN,
   };
   auto header = Header{
       .version = roq::fix::Version::FIX_44,
-      .msg_type = reject.msg_type,
+      .msg_type = new_order_single.msg_type,
       .sender_comp_id = "sender"sv,
       .target_comp_id = "target"sv,
       .msg_seq_num = 1,
-      .sending_time = 1685157508123ms,
+      .sending_time = 1685248384123ms,
   };
-  auto message = reject.encode(header, buffer);
+  auto message = new_order_single.encode(header, buffer);
   REQUIRE(std::size(message) > 0);
   auto tmp = fmt::format("{}"sv, roq::debug::fix::Message{message});
   // note! you can use https://fixparser.targetcompid.com/ to decode this message
   auto expected =
-      "8=FIX.4.4|9=0000084|35=3|49=sender|56=target|34=1|52=20230527-03:18:28.123|45=1|58=failure|372=D|373=11|10=154|"sv;
+      "8=FIX.4.4|9=0000176|35=D|49=sender|56=target|34=1|52=20230528-04:33:04.123|"
+      "11=123|1=A1|55=BTC-PERPETUAL|207=deribit|54=1|60=20230528-04:33:04.123|"
+      "38=1.000000000000|40=2|44=27193.000000000000|59=1|"
+      "10=201|"sv;
   CHECK(tmp == expected);
   fmt::print(stderr, "{}\n"sv, roq::debug::hex::Message{message});
 }
