11e7f3ec4393ca77278eb45e0264a7dc9f874043
thraneh <thraneh@gmail.com>
2023-08-20 14:05:34 +0200

    rename

--- src/roq/fix_proxy/test.sh
+++ src/roq/fix_proxy/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-./fix-bridge-client-simple \
+./roq-fix-proxy \
   --config_file config/test.toml \
   --fix_target_comp_id "roq-fix-bridge" \
   --fix_sender_comp_id "roq-fix-client-test" \
